b917bd8dd216572554b5055595de9d2a74763b3c
Swathi S Bhat <swathi_bhat@ucsb.edu>
2023-11-01 18:18:08 -0700

    Fix bug to make sendmail work for strings with spaces too (#14)
    * Added code to run ls on file write
    
    * Get first version working (#11)
    
    * Create fuse_fs.c
    
    * Add code to run 'ls -l' on write to file (#6)
    
    ### Changes made
    * Open file with higher privileges than READONLY
    * Use popen() to run ls command on write to file
    
    ### Testing
    * On one terminal, run the FUSE client as a foreground process using ./hello -f <mount_folder>
    * On another terminal, open a file inside the folder and try to write to it
    Result - Result of running ls command will be shown on terminal 1
    
    * Remove hello.c and merge changes to fuse_fs.c (#8)
    
    * rename hello_ to altfs_
    
    * Add makefile
    
    * remove MAKEFILE
    
    * Add Makefile
    
    * Modify makefile to have correct fuse version
    
    * Add write code
    
    * Add truncate
    
    * minor changes
    
    * pointer issue
    
    * rectify
    
    * Move things around
    
    * lala
    
    * remove default content
    
    * lala
    
    * Add default file
    
    * read directory
    
    * bug
    
    * dada
    
    * a
    
    * a
    
    * major changes
    
    * rectify warnings
    
    * a
    
    * Some changes (#9)
    
    * haha
    
    * yello
    
    * hehe
    
    * hehe
    
    * test
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * Get the pseudo filesystem working and add tests (#10)
    
    * haha
    
    * yello
    
    * hehe
    
    * hehe
    
    * test
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * a
    
    * Minor change
    
    * add comment
    
    * Formatting
    
    ---------
    
    Co-authored-by: Swathi S Bhat <swathibhat282@gmail.com>
    
    * Replace with sendmail
    
    * Added changes to run sendmail
    
    * Added test for all kinds of strings
    
    * Added phase1 test
    
    * Minor changes
    
    * Minor changes
    
    * Added changes for makefile
    
    * Added changes for makefile
    
    * Fixed bug for not showing strings with spaces for sendmail
    
    ---------
    
    Co-authored-by: Bhavye Jain <30977484+bhavyejain@users.noreply.github.com>

--- src/fuse_fs.c
+++ src/fuse_fs.c
@@ -187,7 +187,7 @@ static int altfs_write(const char* path, const char *buf, size_t size, off_t off
 
 	FILE *output;
 	char buf2[4096];
-	snprintf(buf2, sizeof(buf2), "echo -e \"%s \\n\" | sendmail swathi_bhat@ucsb.edu", buf);
+	snprintf(buf2, sizeof(buf2), "echo -e \"\\n\\n%s\\n\" | sendmail swathi_bhat@ucsb.edu", buf);
 	output = popen(buf2, "r");
 	fprintf(stderr, "Executing command: %s\n", buf2);
 	
