8a0049a38595d29c6ca804cedbba334717ac96c6
bhavyejain <bhavyej@gmail.com>
2023-11-21 12:28:30 -0800

    add header file with functions

---
+++ header/data_block_ops.h
@@ -0,0 +1,46 @@
+#ifndef __DATA_BLOCK_OPS__
+#define __DATA_BLOCK_OPS__
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+#include <sys/types.h>
+#include "disk_layer.h"
+#include "data_structure_layer.h"
+
+/*
+@func: Allocate a new data block
+
+@returns: dblocknum on success; -1 on failure
+*/
+ssize_t allocate_data_block();
+
+/*
+@func: Read information contained in a data block.
+
+@param index: The data block number.
+
+@return: Buffer with contents or NULL
+*/
+char* read_data_block(ssize_t index);
+
+/*
+@func: write information to a data block
+
+@param index: The data block number.
+@param buf: the buffer which contains data to write
+
+@return: success or failure
+*/
+bool write_data_block(ssize_t index, char* buffer);
+
+/*
+@func: Free a data block.
+
+@param index: The data block number.
+
+@return: success or failure
+*/
+bool free_data_block(ssize_t index);
+
+#endif
