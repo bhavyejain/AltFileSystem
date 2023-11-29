8889cc6e3d7a16c450e279a9cdaa811fc1136290
bhavyejain <bhavyej@gmail.com>
2023-11-29 13:57:33 -0800

    Make test helpers and change includes to .h in test_dblock_inode_freelist

--- test/test_dblock_inode_freelist.c
+++ test/test_dblock_inode_freelist.c
@@ -3,52 +3,13 @@
 #include <errno.h>
 #include <stdlib.h>
 
-#include "../src/disk_layer.c"
-#include "../src/superblock_layer.c"
-#include "../src/inode_ops.c"
-#include "../src/data_block_ops.c"
+#include "test_helpers.h"
+#include "../header/disk_layer.h"
+#include "../header/inode_ops.h"
+#include "../header/data_block_ops.h"
+#include "../header/superblock_layer.h"
 
 #define DBLOCK_INODE_FREELIST_TEST "altfs_dblock_inode_freelist_test"
-
-int print_freelist(ssize_t blocknum)
-{   
-    printf("\n******************** FREELIST ********************\n");
-    printf("\n************ FREELIST FOR BLOCK: %ld *************\n",blocknum);
-    char *buff = (char*)malloc(BLOCK_SIZE);
-
-    if (!altfs_read_block(blocknum, buff))
-    {
-        printf("Print freelist: Error reading contents of free list block number: %ld\n",blocknum);
-        return -1;
-    }
-    ssize_t *buff_numptr = (ssize_t *)buff;
-    for (size_t i = 0; i < NUM_OF_ADDRESSES_PER_BLOCK / 8; i++)
-    {
-        for (size_t j = 0; j < 8; j++)
-        {
-            if (i == 0 && j == 0)
-                printf("Next free block: %ld\n", buff_numptr[0]);
-            else
-            printf("%ld ", buff_numptr[i * 8 + j]);
-        }
-        printf("\n");
-    }
-    printf("\n*************************************************\n");
-    return buff_numptr[0];
-}
-
-void print_inode(struct inode** node)
-{
-    printf("\n******************** INODE ********************\n");
-    for(ssize_t i = 0; i < NUM_OF_DIRECT_BLOCKS; i++)
-    {
-        printf("Direct block %ld: %ld\n", i, (*node)->i_direct_blocks[i]);
-    }
-    printf("Single indirect block: %ld\n", (*node)->i_single_indirect);
-    printf("Double indirect block: %ld\n", (*node)->i_double_indirect);
-    printf("Triple indirect block: %ld\n", (*node)->i_triple_indirect);
-    printf("******************** INODE ********************\n\n");
-}
 
 // verify free list state after allocating more than 512 blocks
 // Freelist head should update new freelist block after 512 blocks are allocated
